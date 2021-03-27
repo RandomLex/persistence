69cc659d240e9f2db2d6ed95f9d96cec53a20ada
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-03-27 14:44:36 +0300

    spring-mvc-old-style-mapping

--- app/src/main/java/com/academy/persistence/app/controllers/EmployeeJsonController.java
+++ app/src/main/java/com/academy/persistence/app/controllers/EmployeeJsonController.java
@@ -1,46 +1,35 @@
 package com.academy.persistence.app.controllers;
 
 import com.academy.persistence.app.services.EmployeeService;
-import com.academy.persistence.app.services.EmployeeServiceImpl;
 import com.academy.persistence.model.Employee;
 import lombok.AllArgsConstructor;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
-
-//@WebServlet("/employee")
 
 @Controller
 @AllArgsConstructor
 @RequestMapping(path = "/employee", produces = "application/json;charset=UTF-8")
+@ResponseBody
 public class EmployeeJsonController {
     private static final String ID = "id";
 
     private final EmployeeService service;
 
-
     @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
     public List<Employee> getAll() {
         return service.getAllEmployees();
     }
 
     @RequestMapping(method = RequestMethod.GET, params = "id")
-    @ResponseBody
     public Employee get(@RequestParam int id) {
         Optional<Employee> employee = service.getEmployee(id);
         if (employee.isPresent()) {
@@ -50,32 +39,23 @@ public class EmployeeJsonController {
         }
     }
 
-//    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String idAsString = req.getParameter(ID);
-//        if (idAsString == null) {
-//            toJson(service.getAllEmployees(), resp);
-//        } else {
-//            toJson(service.getEmployee(Integer.parseInt(idAsString)), resp);
-//        }
-//    }
-//
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Employee employee = toObject(Employee.class, req);
-//        toJson(service.saveEmployee(employee), resp);
-//    }
-//
-//    @Override
-//    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Employee employee = toObject(Employee.class, req);
-//        toJson(service.saveEmployee(employee), resp);
-//    }
-//
-//    @Override
-//    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String idAsString = req.getParameter(ID);
-//        if (idAsString != null) {
-//            toJson(service.deleteEmployee(Integer.parseInt(idAsString)), resp);
-//        }
-//    }
+    @RequestMapping(method = RequestMethod.POST)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return service.saveEmployee(employee);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public Employee deleteEmployee(@RequestParam int id) {
+        Optional<Employee> employee = service.deleteEmployee(id);
+        if (employee.isPresent()) {
+            return employee.get();
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee with id " + id);
+        }
+    }
 }

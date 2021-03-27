def024e154da40e62d1b8325173ad279d357db5e
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-03-27 15:16:47 +0300

    04-spring-mvc-java-based-config

---
+++ app/src/main/java/com/academy/persistence/app/config/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.academy.persistence.app.config;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("com.academy.persistence.app")
+@EnableWebMvc
+public class ApplicationConfig {
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver(@Autowired ApplicationContext ctx) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setApplicationContext(ctx);
+        resolver.setPrefix("/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+}

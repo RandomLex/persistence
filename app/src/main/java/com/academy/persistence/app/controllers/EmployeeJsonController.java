f3b42403d22f159317f2f22bf6e6c34dcfd04fd1
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-03-29 21:54:48 +0300

    06-spring-mvc-rest-with-path

--- app/src/main/java/com/academy/persistence/app/controllers/EmployeeJsonController.java
+++ app/src/main/java/com/academy/persistence/app/controllers/EmployeeJsonController.java
@@ -3,10 +3,14 @@ package com.academy.persistence.app.controllers;
 import com.academy.persistence.app.services.EmployeeService;
 import com.academy.persistence.model.Employee;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +22,7 @@ import java.util.List;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping(path = "/employee", produces = "application/json;charset=UTF-8")
+@RequestMapping(path = "/employees", produces = "application/json;charset=UTF-8")
 @ResponseBody
 public class EmployeeJsonController {
     private static final String ID = "id";
@@ -35,18 +39,40 @@ public class EmployeeJsonController {
         return ResponseEntity.of(service.getEmployee(id));
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getByPath(@PathVariable int id) {
+        return ResponseEntity.of(service.getEmployee(id));
+    }
+
     @PostMapping
     public Employee createEmployee(@RequestBody Employee employee) {
         return service.saveEmployee(employee);
     }
 
-    @PutMapping
-    public Employee updateEmployee(@RequestBody Employee employee) {
-        return service.saveEmployee(employee);
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateEmployee(
+            @RequestBody Employee employee,
+            @PathVariable int id) {
+        return employee != null && id != employee.getId()
+                ? createExceptionMessage("Id in path and in entity must be the same")
+                : ResponseEntity.ok(service.saveEmployee(employee));
     }
 
-    @DeleteMapping
-    public ResponseEntity<Employee> deleteEmployee(@RequestParam int id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable int id) {
         return ResponseEntity.of(service.deleteEmployee(id));
+    }
+
+    private ResponseEntity<ExceptionMessage> createExceptionMessage(String msg) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ExceptionMessage.builder()
+                        .message(msg)
+                        .build());
+    }
+
+    @Builder
+    @Getter
+    public static class ExceptionMessage {
+        private final String message;
     }
 }

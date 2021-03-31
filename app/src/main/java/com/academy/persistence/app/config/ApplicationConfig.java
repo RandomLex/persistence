9d2d43377f46eb28254947a15c8b2cb9aaa98cce
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-03-31 21:59:52 +0300

    08-spring-aop-log-aspect

--- app/src/main/java/com/academy/persistence/app/config/ApplicationConfig.java
+++ app/src/main/java/com/academy/persistence/app/config/ApplicationConfig.java
@@ -1,18 +1,19 @@
 package com.academy.persistence.app.config;
 
 
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("com.academy.persistence.app")
 @EnableWebMvc
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
 
     @Bean

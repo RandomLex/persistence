ef0234e530bfceb48ffe92f36569d9ad374b8f1c
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-04-03 15:42:03 +0300

    13-spring-mvc-interceptor-log-headers

--- app/src/main/java/com/academy/persistence/app/controllers/interceptors/RequestLoggingInterceptor.java
+++ app/src/main/java/com/academy/persistence/app/controllers/interceptors/RequestLoggingInterceptor.java
@@ -7,13 +7,30 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @Slf4j
 @Component
 public class RequestLoggingInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-        log.info("{} {}", req.getMethod(), ServletUriComponentsBuilder.fromRequest(req).toUriString());
+        logRequestUrl(req);
+        logHeaders(req);
         return true;
+    }
+
+    private void logHeaders(HttpServletRequest req) {
+        Enumeration<String> headerNames = req.getHeaderNames();
+        if (headerNames.hasMoreElements()) {
+            log.debug("Headers");
+        }
+        while (headerNames.hasMoreElements()) {
+            String name = headerNames.nextElement();
+            log.debug("[{}]: {}", name, req.getHeader(name));
+        }
+    }
+
+    private void logRequestUrl(HttpServletRequest req) {
+        log.info("{} {}", req.getMethod(), ServletUriComponentsBuilder.fromRequest(req).toUriString());
     }
 }
